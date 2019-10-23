74da07830a5e6e8cc5b61fb1c6d5bb97a44129c2
changzhn <changzhenan0212@gmail.com>
2019-10-23 08:35:16 +0800

    format code

--- src/mysql/changzhn/MysqlDemo.java
+++ src/mysql/changzhn/MysqlDemo.java
@@ -9,13 +9,13 @@ public class MysqlDemo {
 //    static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB";
 
     // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB?useSSL=false&serverTimezone=UTC";
 
 
     // 数据库的用户名与密码，需要根据自己的设置
-    static final String USER = "root";
-    static final String PASS = "chang0212";
+    private static final String USER = "root";
+    private static final String PASS = "chang0212";
 
     public static void main(String[] args) {
         Connection conn = null;
